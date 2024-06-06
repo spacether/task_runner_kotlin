9d63568de2e1f63207b5032ff554976bfd24c806
Justin Black <justin.a.black@gmail.com>
2024-06-05 17:18:20 -0700

    Adds queue sending

--- taskpublisher/src/main/kotlin/io/taskpublisher/Main.kt
+++ taskpublisher/src/main/kotlin/io/taskpublisher/Main.kt
@@ -5,6 +5,14 @@ import org.jetbrains.exposed.sql.Database
 import kotlinx.coroutines.*
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
+import com.rabbitmq.client.ConnectionFactory
+
+class Settings {
+    companion object {
+        const val QUEUE_NAME = "tasks"
+        val DELAY_TIME = 1.toDuration(DurationUnit.MINUTES)
+    }
+}
 
 suspend fun publishTask(repository: TaskRepository) {
     val now = LocalDateTime.now()
@@ -13,7 +21,22 @@ suspend fun publishTask(repository: TaskRepository) {
     val tasks = repository.queryTasks(hour, minute)
     print(tasks)
     print("I ran. hour = ${hour} minute=${minute} \n")
+
+    val factory = ConnectionFactory()
+    factory.host = "localhost"
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+
+    channel.queueDeclare(Settings.QUEUE_NAME, true, false, false, null)
+    for (task in tasks) {
+        val message = "Hello World!"
+        channel.basicPublish("", Settings.QUEUE_NAME, null, message.toByteArray(charset("UTF-8")))
+        println(" [x] Sent '$message'")
+    }
+    channel.close()
+    connection.close()
 }
+
 fun configureDatabases() {
     Database.connect(
         "jdbc:postgresql://localhost:5432/taskrunner_db",
@@ -27,6 +50,6 @@ suspend fun main() {
     val repository = DbTaskRepository()
     while (true) {
         publishTask(repository)
-        delay(1.toDuration(DurationUnit.MINUTES))
+        delay(Settings.DELAY_TIME)
     }
 }
